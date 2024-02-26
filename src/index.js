0666143bc86a36367831f341030595eef59c5786
Ranjana34 <106295010+Ranjana34@users.noreply.github.com>
2024-02-26 19:29:47 +0530

    new changes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layout";
 import Home from "./pages/home";
 import Dashboard from "./pages/dashboard";
+import PaymentSummary from "./pages/paymentsummary";
 import './App.css';
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
       <Route index element={<Home />} />
         <Route path="/" element={<Layout />}>
           <Route path="dashboard" element={<Dashboard />} />
-          
+          <Route path="paymentsummary" element={<PaymentSummary />} />
         </Route>
         
       </Routes>
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -3,72 +3,62 @@ import React from "react";
 class Dashboard extends React.Component{
     render() {
        return (
-    <div class="content-body">
-        <div class="container-fluid">
-	      <div class="row">
-					<div class="col-md-4 col-xl-3">
-									<div class="card bg-c-blue order-card">
-										<div class="card-block">
-											<h6 class="m-b-20">Total Users</h6>
-											<h2 class="text-right"><i class="fa fa-cart-plus f-left"></i><span>486</span></h2>
-										</div>
-									</div>
-								</div>
-								<div class="col-md-4 col-xl-3">
-									<div class="card bg-c-green order-card">
-										<div class="card-block">
-											<h6 class="m-b-20">Total Users</h6>
-											<h2 class="text-right"><i class="fa fa-rocket f-left"></i><span>486</span></h2>
-											
-										</div>
-									</div>
-								</div>
-								
-								<div class="col-md-4 col-xl-3">
-									<div class="card bg-c-yellow order-card">
-										<div class="card-block">
-											<h6 class="m-b-20">Total Users</h6>
-											<h2 class="text-right"><i class="fa fa-refresh f-left"></i><span>486</span></h2>
-											
-										</div>
-									</div>
-								</div>
-								
-								<div class="col-md-4 col-xl-3">
-									<div class="card bg-c-pink order-card">
-										<div class="card-block">
-											<h6 class="m-b-20">Total Users</h6>
-											<h2 class="text-right"><i class="fa fa-credit-card f-left"></i><span>486</span></h2>
-											
-										</div>
-									</div>
-								</div>		
+    <div class="mycont">
+		<div id="myModal" class="modal fade" role="dialog">
+				<div class="modal-dialog">
+					<div class="modal-content">
+					<div class="modal-header">
+						<h4 class="modal-title">Payment</h4>
+					</div>
+					<div class="modal-body">
+							<div class="form-group">
+								<label>Mobile Number</label>
+								<input type="text" class="form-control"/>
+							</div>
+							<div class="form-group">
+								<label>Amount</label>
+								<input type="text" class="form-control"/>
+							</div>
+							<div class="col-md-12">
+							<a href="paymentsummary"><button class="btn btn-success">Proceed</button></a>
+							</div>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+					</div>
+					</div>
 				</div>
+				</div>
+	<div class="row">
+					<div class="col-md-12">
+					<img class="topbanner" src="assets/images/top.jpg" alt="Top Banner"/>
+					</div>
+				</div>
+        <div class="container-fluid">
 				<div class="row">
 					<div class="col-md-4 col-xl-3">
 						<div class="logo-maker text-center">
-							<img class="logo-auth" src="assets/images/bpc.jpg" alt=""/>
+							<img data-toggle="modal" data-target="#myModal"  class="logo-auth" src="assets/images/bpc.jpg" alt=""/>
 						</div>
 					</div>
 					<div class="col-md-4 col-xl-3">
 					<div class="logo-maker text-center">
-					<img class="logo-auth" src="assets/images/dstv.png" alt=""/>
+					<img data-toggle="modal" data-target="#myModal" class="logo-auth" src="assets/images/dstv.png" alt=""/>
 					</div>
 					</div>
 					<div class="col-md-4 col-xl-3">
 					<div class="logo-maker text-center">
-					<img class="logo-auth" src="assets/images/mascom.png" alt=""/>
+					<img data-toggle="modal" data-target="#myModal" class="logo-auth" src="assets/images/mascom.png" alt=""/>
 					</div>
 					</div>
 					<div class="col-md-4 col-xl-3">
 					<div class="logo-maker text-center">
-					<img class="logo-auth" src="assets/images/orange.jpeg" alt=""/>
+					<img data-toggle="modal" data-target="#myModal" class="logo-auth" src="assets/images/orange.jpeg" alt=""/>
 					</div>
 					</div>		
 				</div>
 			</div>
     </div>
-	
     );
 }
 }
